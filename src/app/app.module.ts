9f2e34609e750705b8bc050707e2afa0d8da7259
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-04-14 09:18:05 -0500

    initial scaffolding for app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotfoundComponent,
+    DashboardComponent,
+    BreadcrumbComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
