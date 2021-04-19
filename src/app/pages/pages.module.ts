2fca14f134430054c0b95abc9d92175fb481981e
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-04-19 09:41:09 -0500

    add progress functionality to control progressBar behavior

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -26,6 +27,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule,
+    FormsModule,
     RouterModule
   ]
 })
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: [ './progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
 
+  progress: number = 0;
   constructor() { }
 
-  ngOnInit(): void {
+  get percentWidth() {
+    return  `${ this.progress }%`;
+  }
+
+  changeValue( value: number ) {
+
+    if ( this.progress >= 100 && value >= 0 ) {
+      return this.progress = 100;
+    }
+
+    if ( this.progress <= 0 && value < 0 ) {
+      return this.progress = 0;
+    }
+
+    this.progress = this.progress + value;
   }
 
 }
