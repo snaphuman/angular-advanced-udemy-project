888e05fa23866f6ba889749617938c7accfd0b0f
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-04-22 14:36:42 -0500

    apply working selector to selected theme

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -11,18 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  element = document.querySelector('#theme');
+  link: Element = document.querySelector('#theme');
+  links: NodeListOf<Element>;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.link = document.querySelector('#theme');
+    this.links = document.querySelectorAll('.selector');
+    this.checkWorkingTheme();
   }
 
   changeTheme( theme: string ) {
 
     const url = `./assets/css/colors/${ theme }.css`;
 
-    this.element.setAttribute('href', url);
+    this.link.setAttribute('href', url);
     localStorage.setItem('theme', url);
+
+    this.checkWorkingTheme();
+  }
+
+  checkWorkingTheme () {
+
+    let links = this.links;
+
+    links.forEach( element => {
+
+      element.classList.remove('working');
+
+      const btnTheme = element.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
+      const currentTheme = this.link.getAttribute('href');
+
+      if (btnThemeUrl === currentTheme) {
+        element.classList.add('working');
+      }
+    })
+
   }
 }
