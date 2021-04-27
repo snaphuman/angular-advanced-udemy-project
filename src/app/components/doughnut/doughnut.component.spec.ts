c354a937a6fd975e99542a0aed516e5c3c836f56
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-04-27 16:46:43 -0500

    ensure that defined tests are green

--- src/app/components/doughnut/doughnut.component.spec.ts
+++ src/app/components/doughnut/doughnut.component.spec.ts
@@ -1,25 +1,41 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DoughnutData } from 'src/app/models/doughnut-data.model';
 
 import { DoughnutComponent } from './doughnut.component';
 
+@Component({
+  template: `
+    <app-doughnut [dougnutData]="chart"></app-doughnut>
+    `
+})
+class TestHostComponent {
+  chart: DoughnutData = {
+    title: 'Sales',
+    labels: ['download sales', 'in-store sales', 'mail-order sales'],
+    chartData: [[350, 450, 100]] };
+}
+
 describe('DoughnutComponent', () => {
-  let component: DoughnutComponent;
-  let fixture: ComponentFixture<DoughnutComponent>;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DoughnutComponent ]
+      declarations: [ DoughnutComponent, TestHostComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DoughnutComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
+
     expect(component).toBeTruthy();
   });
 });
