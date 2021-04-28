b79141656094b5d5060185b668bb37622c4fd538
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-04-28 14:52:23 -0500

    unsubscribe router arguments when breadcrumbs are not present

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
@@ -8,24 +9,29 @@ import { filter, map } from 'rxjs/operators';
   styles: [
   ]
 })
-export class BreadcrumbComponent {
+export class BreadcrumbComponent implements OnDestroy {
 
   title: string;
+  title$: Subscription;
 
   constructor( private router: Router) {
-
-    this.router.events
-      .pipe(
-        filter(event => event instanceof ActivationEnd),
-        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
-        map((event: ActivationEnd) => event.snapshot.data)
-      )
-
+    this.title$ = this.routeArguments()
       .subscribe( ({ title })  => {
         this.title = title;
         document.title = title
       })
   }
 
+  ngOnDestroy(): void {
+    this.title$.unsubscribe();
+  }
 
+  routeArguments() {
+    return this.router.events
+      .pipe(
+        filter(event => event instanceof ActivationEnd),
+        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+        map((event: ActivationEnd) => event.snapshot.data)
+      )
+  }
 }
