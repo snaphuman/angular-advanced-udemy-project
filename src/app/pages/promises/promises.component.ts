b9a422911e4ba7e9e662f5b813f73f3bc5bdd0b3
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-04-28 10:02:48 -0500

    implement first promises demo

--- src/app/pages/promises/promises.component.ts
+++ src/app/pages/promises/promises.component.ts
@@ -11,6 +11,38 @@ export class PromisesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    this.getUsers().then(users => {
+      console.log(users);
+    });
+    // const promise = new Promise( (resolve, reject) => {
+
+    //   if (false) {
+    //     resolve("Hello world");
+    //   } else {
+    //     reject("Something went wrong");
+    //   }
+    // });
+
+    // promise.then((msg) => {
+
+    //   console.log('Hey im done', msg);
+    // })
+    // .catch( error => {
+
+    //   console.log('Error in promise', error);
+    // })
+
+    // console.log('Init ends');
+  }
+
+  getUsers() {
+
+    return new Promise( resolve => {
+      fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then(body => resolve(body.data))
+    })
   }
 
 }
