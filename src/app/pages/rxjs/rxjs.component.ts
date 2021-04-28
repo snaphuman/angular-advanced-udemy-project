dd89bbb9fbad7e22d26558c990c236414a2ee726
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-04-28 12:04:06 -0500

    replace observable demo by using interval defined in rxjs

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
-
+import { Observable, interval } from 'rxjs';
+import { map, retry, take } from 'rxjs/operators';
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
@@ -11,33 +10,48 @@ import { retry } from 'rxjs/operators';
 export class RxjsComponent {
 
   constructor() {
-    this.numberStream().pipe(
-      retry(2)
-    )
-    .subscribe(
-      val => console.log('subs:', val),
-      err => console.warn('there was an error', err),
+
+    this.interval().subscribe(
+      val => console.log(val),
+      err => console.error(err),
       () => console.info('Observer finished')
     );
+
+    // this.numberStream().pipe(
+    //   retry(2)
+    // )
+    // .subscribe(
+    //   val => console.log('subs:', val),
+    //   err => console.warn('there was an error', err),
+    //   () => console.info('Observer finished')
+    // );
   }
 
-  numberStream(): Observable<number> {
-    let i = -1;
-    return new Observable<number> ( observer => {
-
-      const interval = setInterval( () => {
-        i++;
-        observer.next(i)
-
-        if (i === 8) {
-          observer.error("Error on 8");
-        }
-        if (i === 10) {
-          observer.complete()
-          clearInterval(interval);
-        }
-      }, 1000)
-    });
+  interval(): Observable<number> {
+    return interval(1000)
+          .pipe(
+          take(4),
+          map(value => value +1)
+          );
   }
+
+  // numberStream(): Observable<number> {
+  //   let i = -1;
+  //   return new Observable<number> ( observer => {
+
+  //     const interval = setInterval( () => {
+  //       i++;
+  //       observer.next(i)
+
+  //       if (i === 8) {
+  //         observer.error("Error on 8");
+  //       }
+  //       if (i === 10) {
+  //         observer.complete()
+  //         clearInterval(interval);
+  //       }
+  //     }, 1000)
+  //   });
+  // }
 
 }
