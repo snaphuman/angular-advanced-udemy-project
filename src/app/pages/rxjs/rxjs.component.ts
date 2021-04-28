236c5c374c7ff670476803f2484efc32180a7d36
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-04-28 11:03:45 -0500

    implement first demo of observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -6,11 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent {
 
-  constructor() { }
+  constructor() {
+    this.numberStream().pipe(
+      retry(2)
+    )
+    .subscribe(
+      val => console.log('subs:', val),
+      err => console.warn('there was an error', err),
+      () => console.info('Observer finished')
+    );
+  }
 
-  ngOnInit(): void {
+  numberStream(): Observable<number> {
+    let i = -1;
+    return new Observable<number> ( observer => {
+
+      const interval = setInterval( () => {
+        i++;
+        observer.next(i)
+
+        if (i === 8) {
+          observer.error("Error on 8");
+        }
+        if (i === 10) {
+          observer.complete()
+          clearInterval(interval);
+        }
+      }, 1000)
+    });
   }
 
 }
