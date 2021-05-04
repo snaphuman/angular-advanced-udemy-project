759fab4435382b17e516ba9a8a7a077f19e83e32
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-05-04 15:33:28 -0500

    add auth guard for dashboard page

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { LoginForm } from '../interfaces/login-form.interface';
 import { RegisterForm, RegisterFormBackend } from '../interfaces/register-form.interface';
@@ -13,6 +14,23 @@ const base_url = environment.base_url;
 export class UserService {
 
   constructor( private http: HttpClient) { }
+
+  validateToken(): Observable<boolean> {
+
+    const token = localStorage.getItem('token') || '';
+
+    return this.http.get(`${ base_url }/login/renew`, {
+      headers: {
+        'x-token': token
+      }
+    }).pipe(
+      tap( (res:any) => {
+        localStorage.setItem('token', res.token);
+      }),
+      map( (res:any) => true ),
+      catchError( error => of (false) )
+    );
+  }
 
   createUser( formData: RegisterForm ) {
     console.log('creating user');
