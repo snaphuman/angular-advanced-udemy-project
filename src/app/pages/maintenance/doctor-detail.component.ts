de85acc0ee273c0f7ebdf8b959c1f15b8832f6af
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-06-02 18:33:27 -0500

    show hospitals in selector options

--- src/app/pages/maintenance/doctor-detail.component.ts
+++ src/app/pages/maintenance/doctor-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from 'src/app/models/hospital-model';
+import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
   selector: 'app-doctor-detail',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorDetailComponent implements OnInit {
 
-  constructor() { }
+  doctorForm: FormGroup;
+  hospitals: Hospital[] = [];
+
+  constructor(private fb: FormBuilder,
+              private hospitalService: HospitalService) { }
 
   ngOnInit(): void {
+    this.doctorForm = this.fb.group({
+      nombre: ['Person', Validators.required],
+      hospital: ['', Validators.required]
+    });
+
+    this.showHospitals();
   }
+
+  saveDoctor() {
+
+    console.log(this.doctorForm.value);
+  };
+
+  showHospitals() {
+
+    this.hospitalService.showHospitals()
+        .subscribe( (res: Hospital[]) => {
+          this.hospitals = res;
+        })
+  }
+
 
 }
